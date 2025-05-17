eade9aca846aefa7c2a2c9df59fb9e76e5382de3
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-17 23:23:46 +0200

    3

--- src/components/Main/WeatherList/WeatherCard/WeatherCard.jsx
+++ src/components/Main/WeatherList/WeatherCard/WeatherCard.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-const WeatherCard = ({data,remove}) => {
-  const {title, description, price, img_url} = data;
+const WeatherCard = ({data}) => {
+  const {date, time, degrees} = data;
   return <article >
-    <h4>{title}</h4>
-    <p>{description}</p>
-    <p>Precio: {price}€</p>
-    <img className="picture_item" src={img_url} alt={title} />
-    <button onClick={remove}>Borrar</button>
+    <p>Temperatura: {degrees}</p>
+    <p>Fecha: {date}</p>
+    <p>Hora: {time}</p>
   </article>;
 };
 
--- src/components/Main/WeatherList/WeatherList.jsx
+++ src/components/Main/WeatherList/WeatherList.jsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import axios from 'axios';
+import WeatherCard from "./WeatherCard/WeatherCard";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const WeatherList = () => {
@@ -7,22 +8,27 @@ const WeatherList = () => {
   const [city, setCity] = useState("Madrid");
   const [weatherListItem, setWeatherListItem] = useState([]);
   
+  const inputRef = useRef(""); // esta referencia será un string vacío al principio
+ 
   useEffect(() => {
-    async function fetchData() {
-      try{
-        // Petición HTTP
-        const res = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
-        const json = res.data;
-
-        // Guarda en el array de datos el resultado. Procesa los datos
-        setWeatherListItem(res.data.list.filter(c => c.main.temp));
-      }catch(e){
-        setWeatherListItem([]) // No pintes nada 
-      }
-    }
-
-    fetchData();
-  }, [city]); 
+      async function fetchData() {
+         try{
+       // Petición HTTP
+          const res = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
+          const json = await res.data.list;
+        
+          // Guarda en el array de posts el resultado. Procesa los datos
+          setWeatherListItem(json);
+            }catch(e){
+          setWeatherListItem([]) // No pintes nada 
+          }
+        } fetchData(); 
+      }, [city]);
+  
+  const paintData = () =>
+    weatherListItem.map((item, index) => (
+      <WeatherCard key={index} data={item} remove={() => removeItem(index)} />
+    ));
 
 
   const handleSubmit = e => {
@@ -32,22 +38,15 @@ const WeatherList = () => {
   };
 
   return <section className="city-section">
-  <h1>Weather list</h1>
-  <form onSubmit={handleSubmit}>
-    <input name="city"/>
-    <button>Buscar</button>
-  </form>
+  
+    <h1>Weather list</h1>
+    <form onSubmit={handleSubmit}>
+      <input name="city"/>
+      <button>Buscar</button>
+    </form>
 
-  {weatherListItem.length!==0?
-                    <ul className='topics'>
-                      {weatherListItem.map(item => (
-                        <li key={item.dt}>
-                        {new Date(item.dt_txt).toLocaleString()} - {(item.main.temp - 273.15).toFixed(1)}°C
-                      </li>
-                      ))}
-                    </ul>
-                    :""
-  }
+    {paintData()}
+
 </section>
 };
 
